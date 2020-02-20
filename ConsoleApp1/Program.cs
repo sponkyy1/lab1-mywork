7e3ae2572fc4958cdd5914cd51c4744a608df4e7
name <name@gmail.com>
2020-02-20 07:31:06 +0200

    homewor project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static public int Example(int a)
+        {
+            int n = a;
+            Console.Write(n % 10);
+            while ((n /= 10) != 0)
+            {
+                Console.Write(n % 10);
+            }
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+            Example(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 654;
+            int result = ConsoleApp1.Program.Example(a);
+            Assert.AreEqual(654, result);
+        }
+    }
+}
